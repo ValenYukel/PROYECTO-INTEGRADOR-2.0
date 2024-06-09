fcf5b4ebf103f2df1271980208918289b9758f6f
ValenYukel <vyukelson@udesa.edu.ar>
2024-06-08 23:13:34 -0300

    pco

--- js/category.js
+++ js/category.js
@@ -1,46 +1,14 @@
 
-let menu = document.querySelector("#menu")
-console.log(menu)
+let categoryLinks = document.querySelector("#menu a")
 
-let queryString = window.location.search;
-let urlParams = new URLSearchParams(queryString);
-let categoria = urlParams.get('class');
+categoryLinks.addEventListener("click",function(){ 
+    
 
-let url = 'https://fakestoreapi.com/products/category/${categoria}';
+});
 
-let hombres = document.querySelectorAll(".remera-hombre");
 
-fetch(url)
 
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(data){
 
-        let resultados = data;
-        console.log(resultados);
-        
-        for(let i = 0; i < resultados.length; i++) {
-            if (i < hombres.length) { 
-                let nombreProd = resultados[i].title;
-                let descripcionProd = resultados[i].description;
-                let precioProd = resultados[i].price;
-                let imagenProd = resultados[i].image;
-                
-                let nombre = hombres[i].querySelector(".nombre-producto");
-                let descripcion = hombres[i].querySelector(".descripcion-producto");
-                let precio = hombres[i].querySelector(".precio-producto");
-                let imagen = hombres[i].querySelector(".producto-imagen");
-                
-                nombre.innerText = nombreProd;
-                descripcion.innerText = descripcionProd;
-                precio.innerText = `$${precioProd}`;
-                imagen.src = imagenProd;
-            }
-        }
+let url = 'https://fakestoreapi.com/products/category/';
 
-    })
-    .catch(function(e){
-        console.log(e);
-        console.log("Lo sentimos, este item no se muestra correctamente, refresque la página e intente nuevamente.");
-    });
+
