6b0d633c3efc1b1fb19ee455e447bf6f1bd442df
GeronimoTiscornia <gero.tisco@gmail.com>
2024-06-18 19:08:00 -0300

    terminando el cart

--- js/archivoCart.js
+++ js/archivoCart.js
@@ -11,12 +11,32 @@ fetch(url)
         let recuperarStorage = lacalStorage.getItem('');
         let productosRecuperados = json.parse();
 
-        if (recuperarStorage === null) {
+        if (!productosRecuperados.length === 0) {
             let detalle = document.querySelector('.detlle');
             detalle.innerHTML = "Su carrito esta vacio"
         }
-        else if (recuperarStorage) {
-            
+        else {
+            // Si hay productos en el carrito, recuperar los IDs y consultar la API
+            let lista = document.querySelector('.contenedor');
+
+            productosRecuperados.forEach(id => {
+                let producto = resultados.find(item => item.id === id);
+
+                if (producto) {
+                    let productoDiv = document.createElement('div');
+                    productoDiv.className = 'producto';
+                    productoDiv.innerHTML = `
+                        <div class="foto-producto">
+                            <img src="${producto.image}" alt="${producto.title}">
+                        </div>
+                        <div class="detalle">
+                            <p>Nombre: ${producto.title}</p>
+                            <p>Descripción: ${producto.description}</p>
+                            <p>Precio: $${producto.price}</p>
+                        </div>
+                    `;
+                }
+            });
         }
     })
     .catch(function(error){
